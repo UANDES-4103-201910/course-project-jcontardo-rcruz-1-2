9c11b04b4873bee5e820157135e16a183932d832
rcruz <rcruz@miuandes.cl>
2019-06-02 22:58:08 -0400

    devise y home

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
   resource :post
   resource :comment
   resource :user
-
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
