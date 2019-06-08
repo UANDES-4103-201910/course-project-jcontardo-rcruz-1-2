190775ef8d94dd05e4642ce0fe877e03e24d2bd4
rcruz <rcruz@miuandes.cl>
2019-06-08 17:54:32 -0400

    comitiando

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,11 @@ ActiveRecord::Schema.define(version: 2019_06_08_194742) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_models_on_email", unique: true
@@ -90,7 +95,7 @@ ActiveRecord::Schema.define(version: 2019_06_08_194742) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password"
   end
 
 end
