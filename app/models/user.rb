fc7e0358101386d598fe4153e33b1a0cb0f62541
JaimeContardo <jcontardo@miuandes.cl>
2019-06-09 12:21:35 -0400

    Google Authentication working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  def self.from_omniauth(auth)
+  	puts auth.info.inspect
+    where(email: auth.info.email).first_or_initialize.tap do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name
+      user.password = "123456789"
+      user.save!
+    end
+  end
 end
